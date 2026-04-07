5f4f9380f10624169e36b0aaf6cc5f85e25505cf
Lorence G. Suizo <l.suizo.563419@umindanao.edu.ph>
2026-04-07 15:22:31 +0800

    Add files via upload

--- Suizo.java
+++ Suizo.java
@@ -2,102 +2,127 @@ package Suizo;
 
 import java.util.*;
 
-
 public class Suizo {
 
 	public static void main(String[] args) {
-	
-	Scanner sc = new Scanner(System.in);
-	
-	
-	int [][] num = {{120,250,789,7},{58,79,31,87},{8,1,2,3},{99,98,97,96}};
-	
-	for (int i =0; i< num.length; i++) {
-		for(int j=0; j<num[i].length; j++) {
-			System.out.printf("%5d",num[i][j]);
-		}
 		
-	System.out.println();	
-	}
+		System.out.print("-----------------------------------\n");
+		System.out.print("   Hotel Room Reservation System\n");
+		System.out.print("-----------------------------------\n");
+		
+		Scanner sc = new Scanner(System.in);
+		
+		
+		int [][] hotel = new int [7] [5];
+		boolean exit = true;
+		
+		
+		
+	while(exit) {
 	
-	
-	for (int i =0; i< num.length; i++) {
-		for(int j=0; j<num[i].length; j++) {
-		if(num[i][j] == 1) {
-			System.out.println("\nNumber 1 is found at indices " + i + " and " + j + ".");
-			num[i][j] = 50;	
-		}
+	System.out.print(  	  "\nMENU\n"
+						+ "[1]View Rooms\n"
+						+ "[2]Check In\n"
+						+ "[3]Check Out\n"
+						+ "[4]Exit\n"
+						+ "\nSelect: " );
+	int option = sc.nextInt();sc.nextLine();
+		
+	if(option >= 1 && option <=4) {
+		
+		
+		switch(option) {
+		
+		case 1:
+			
+			System.out.println("\n0 = Available 1 = Not Available\n");
+			for (int i =0; i< hotel.length; i++) {
+				System.out.print("Floor " + (i+1) +" : ");
+				for(int j=0; j<hotel[i].length; j++) {
+				System.out.print("[" + hotel[i][j] + "]");
+			}
+			
+		System.out.println();	
 		}	
-	}
-	System.out.println();
-	
+		break;
+		
+		case 2:
+			boolean check = true;
+		
+		while(check) {
+		
 
-	for (int i =0; i< num.length; i++) {
-		for(int j=0; j<num[i].length; j++) {
-			System.out.printf("%5d",num[i][j]);
-	
-	
+			System.out.print("Enter floor (1-7): ");
+			int floor = sc.nextInt() -1 ;
+			
+			System.out.print("Enter room (1-5): ");
+			int room = sc.nextInt() -1 ; sc.nextLine();
+			
+			if(hotel [floor][room] == 0) {
+			   hotel [floor][room] = 1;
+			   System.out.println("Check-in successful!");
+			   
+			   check = false;
+			}
+			
+		 	else {
+			System.out.print("\nRoom already occupied.\n");
+			System.out.print("Pls Reselect Floor and Room No.\n");
+			
+			}
+			
+			
 		}
-		System.out.println();
+			
+		break;
+		
+		case 3:
+			boolean check1 = true;
+			
+		while(check1) {
+
+			System.out.print("Enter floor (1-7): ");
+			int nfloor = - 1 + sc.nextInt();
+			
+			System.out.print("Enter room (1-5): ");
+			int nroom = - 1 + sc.nextInt(); sc.nextLine();
+			
+			if(hotel [nfloor][nroom] == 1) {
+			   hotel [nfloor][nroom] = 0;
+			   System.out.println("Check-out successful!");
+			   check1 = false;
+			}
+			else {
+			System.out.print("Room already empty\n");
+			System.out.print("Pls Reselect Floor and Room No.\n");	
+			}
+				}
+			
+		break;
+		
+		case 4:
+		
+			System.out.print("...Thank you for staying...");
+			exit = false;
+			sc.close();
+			
+		break;
+		
 	}
-	
-	for (int i =0; i< num.length; i++) {
-		for(int j=0; j<num[i].length; j++) {
-			num[1][j] = 0;	
+		
+	}else{
+		System.out.print("Invalid Input. Try Again.\n");
+		continue;
+	}
+		
+		
 		}
 		
-	}	
-	System.out.println();
-	
-
-	for (int i =0; i< num.length; i++) {
-		for(int j=0; j<num[i].length; j++) {
-			System.out.printf("%5d",num[i][j]);
-	
-	
-		}
-		System.out.println();
-	}
-
-	
-	
+			
 		
-		
-//	System.out.print("Enter Number of Rows: ");
-//	int a = sc.nextInt();
-//	
-//	System.out.print("Enter Number of Column: ");
-//	int b = sc.nextInt();
-//	
-//	int [][] nums = new int [a]	[b];
-//		
-//	for (int i =0; i< nums.length; i++) {
-//		for(int j=0; j<nums[i].length; j++) {
-//			System.out.print("Enter numbers: ");
-//			nums[i][j] = sc.nextInt();
-//			
-//		}	
-//	}	
-//		
-//	for (int i =0; i< nums.length; i++) {
-//		for(int j=0; j<nums[i].length; j++) {
-//			System.out.print(nums[i][j] + " ");
-//			 	
-//		}
-//		System.out.println("");
-//	}		
-//		
-//		
-//		
-//		
+}
 	
 
-	
-	
-	
-	
-	
-	}
-	
-	
-	}
+}
+
+
